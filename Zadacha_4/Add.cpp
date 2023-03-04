e1a397990a1858286415e9261a7c852b44d0f049
aleksandr.ilyin <sasha8@list.ru>
2023-03-04 15:08:09 +0300

    Задача 4 выполнена

---
+++ Zadacha_4/Add.cpp
@@ -0,0 +1,6 @@
+
+int a = 10;
+int b = 12;
+int c = 50;
+int d = 4;
+
---
+++ Zadacha_4/Zadacha_4.cpp
@@ -0,0 +1,14 @@
+﻿// ** Написать программу, вычисляющую выражение из первого задания, 
+// а переменные для неё объявлены и инициализировать в другом cpp файле.Используйте extern.
+
+#include <iostream>
+
+extern int a, b, c, d;
+
+int main()
+{
+  
+    double result = a * (b + static_cast<double>(c) / static_cast<double>(d));
+
+    std::cout << "Rezult: " << result << std::endl;
+}
