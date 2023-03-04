fcb7c7a1170231d155d1c30324f0afa0baabcfbd
aleksandr.ilyin <sasha8@list.ru>
2023-03-04 12:05:47 +0300

    Задача 1 выполнена

---
+++ Zadacha_1/Zadacha 1.cpp
@@ -0,0 +1,20 @@
+﻿// Написать программу, вычисляющую выражение a* (b + (c / d)) и выводящую результат с плавающей точкой, 
+// где a, b, c, d – целочисленные константы.
+// Используйте static_cast или С - Style cast к float чтобы выполнить точное деление.
+
+
+#include <iostream>
+
+int main()
+{
+    int a, b, c, d;
+    a = 10;
+    b = 12;
+    c = 50;
+    d = 4;
+
+    double result = a * (b + static_cast<double>(c) / static_cast<double>(d));
+
+    std::cout << "Rezult: " << result << std::endl;
+}
+
